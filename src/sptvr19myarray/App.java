a726d9380968f2a28fc139bf4cdcd3f7b77c0d99
roman ivanov <roman.ivanov@ivkhk.ee>
2020-09-16 11:24:56 +0300

    добавили среднее арифметическое

--- src/sptvr19myarray/App.java
+++ src/sptvr19myarray/App.java
@@ -16,6 +16,9 @@ class App {
         System.out.println("---MyArray---");
         Random random = new Random();
         int rnum = 0;
+        int min = 100;
+        int max = 0;
+        int sum = 0;
         //Создаем массив целых четных чисел
         int[] numbers = new int[20];
         for (int i = 0; i < numbers.length; i++) {
@@ -26,8 +29,13 @@ class App {
                     break;
                 }
             }
+            if(numbers[i] > max)max=numbers[i];
+            if(numbers[i] < min)min=numbers[i];
+            sum += numbers[i];
             System.out.printf("%4d",numbers[i]);
         }
         System.out.println();
+        System.out.println("Среднее арифметическое без учета минимального ("+min+") и максимального ("+max+"):");
+        System.out.printf("%.2f%n",(double)(sum-min-max)/(numbers.length - 2));
     }
 }
